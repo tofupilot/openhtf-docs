7bb6735d9d282c9b0d7d594491ead7aba83ddff3
Felix <felix.berthier@epita.fr>
2024-09-05 11:29:50 +0200

    Updated metadatas

--- script.py
+++ script.py
@@ -1,65 +1,30 @@
 import openhtf as htf
-from openhtf.plugs.user_input import UserInput
 from openhtf.output.callbacks import json_factory
-
-
-# Define the test phases
-@htf.measures(htf.Measurement("operator_name").doc("Name of the operator"))
-@htf.plug(user_input=UserInput)
-def ask_operator_name(test, user_input):
-    operator_name = user_input.prompt(message="Please enter your name:")
-    test.measurements.operator_name = operator_name
+from openhtf.util import validators
 
 
 @htf.measures(
-    htf.Measurement("test_voltage").doc("Voltage to test (in volts)").in_range(0, 240)
+    "frequency",
+    docstring="Frequency measurement",
+    units=htf.units.HERTZ,
+    validators=[validators.in_range(0, 10)],
 )
-@htf.plug(user_input=UserInput)
-def ask_test_voltage(test, user_input):
-    test_voltage = user_input.prompt(
-        message="Please enter the test voltage (in volts):", text_input=False
+@htf.measures("status", docstring="Status message")
+def inline_phase(test):
+    test.measurements.frequency = 15  # Hertz (out of valid range)
+    test.measurements.status = "This one is unvalidated."
+
+
+def main():
+    test = htf.Test(inline_phase)
+    test.add_output_callbacks(
+        json_factory.OutputToJSON(
+            "test_result2.json",  # The name of the output file
+            indent=2,  # Pretty-printing the JSON with an indentation of 2 spaces
+        )
     )
-    try:
-        test_voltage = float(test_voltage)
-        test.measurements.test_voltage = test_voltage
-    except ValueError:
-        test.logger.error("Invalid voltage entered, must be a number.")
-        return htf.PhaseResult.FAIL_AND_CONTINUE
-
-
-@htf.measures(htf.Measurement("led_color").doc("LED color to test"))
-@htf.plug(user_input=UserInput)
-def ask_led_color(test, user_input):
-    led_color = user_input.prompt(
-        message="Please select the LED color to test (Red/Green/Blue):"
-    )
-    if led_color not in ["Red", "Green", "Blue"]:
-        test.logger.error("Invalid color selected.")
-        return htf.PhaseResult.FAIL_AND_CONTINUE
-    test.measurements.led_color = led_color
-
-
-@htf.measures(htf.Measurement("confirmation").doc("Operator confirmation"))
-@htf.plug(user_input=UserInput)
-def ask_confirmation(test, user_input):
-    confirmation = user_input.prompt(
-        message='Type "CONFIRM" to confirm the above information:'
-    )
-    if confirmation != "CONFIRM":
-        test.logger.error("Operator did not confirm the information.")
-        test.measurements.confirmation = False
-        return htf.PhaseResult.FAIL_AND_CONTINUE
-    test.measurements.confirmation = True
-
-
-# Define the test
-def user_input_test():
-    return htf.Test(
-        ask_operator_name, ask_test_voltage, ask_led_color, ask_confirmation
-    )
+    test.execute(lambda: "DeviceUnderTest123")
 
 
 if __name__ == "__main__":
-    test = user_input_test()
-    test.add_output_callbacks(json_factory.OutputToJSON("./test_output.json"))
-    test.execute(test_start=lambda: "test-uid-1234")
+    main()

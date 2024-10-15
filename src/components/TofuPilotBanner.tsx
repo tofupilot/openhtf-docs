6a06151a50f8a7b70e05f1a4d2261cdb79c442b6
Julien Buteau <julien.buteau@gmail.com>
2024-10-15 21:39:44 +0200

    Updated TofuPilot banner

--- src/components/TofuPilotBanner.tsx
+++ src/components/TofuPilotBanner.tsx
@@ -53,32 +53,10 @@ export const BackgroundGradient = ({
           backgroundSize: animate ? '400% 400%' : undefined,
         }}
         className={cn(
-          'absolute inset-0 z-[1] rounded-3xl opacity-60 blur-xl transition duration-500 will-change-transform group-hover:opacity-100',
+          'absolute inset-0 z-[1] rounded-3xl opacity-40 blur-md transition duration-500 will-change-transform group-hover:opacity-100 group-hover:blur-lg',
           'bg-[radial-gradient(circle_farthest-side_at_0_100%,#00ccb1,transparent),radial-gradient(circle_farthest-side_at_100%_0,#7b61ff,transparent),radial-gradient(circle_farthest-side_at_100%_100%,#ffc414,transparent),radial-gradient(circle_farthest-side_at_0_0,#1ca0fb,#141316)]',
         )}
       />
-      <motion.div
-        variants={animate ? variants : undefined}
-        initial={animate ? 'initial' : undefined}
-        animate={animate ? 'animate' : undefined}
-        transition={
-          animate
-            ? {
-                duration: 5,
-                repeat: Infinity,
-                repeatType: 'reverse',
-              }
-            : undefined
-        }
-        style={{
-          backgroundSize: animate ? '400% 400%' : undefined,
-        }}
-        className={cn(
-          'absolute inset-0 z-[1] rounded-3xl will-change-transform',
-          'bg-[radial-gradient(circle_farthest-side_at_0_100%,#00ccb1,transparent),radial-gradient(circle_farthest-side_at_100%_0,#7b61ff,transparent),radial-gradient(circle_farthest-side_at_100%_100%,#ffc414,transparent),radial-gradient(circle_farthest-side_at_0_0,#1ca0fb,#141316)]',
-        )}
-      />
-
       <div className={cn('relative z-10', className)}>{children}</div>
     </div>
   )
@@ -92,10 +70,10 @@ export function TofuPilotBanner() {
   }
 
   return (
-    <div className="fixed bottom-0 left-0 z-50 px-4 pb-4">
+    <div className="fixed bottom-0 left-0 z-50 select-none px-4 pb-4">
       <div className="block h-[144px]">
-        <BackgroundGradient className="w-[280px] rounded-[22px] bg-white dark:bg-zinc-900">
-          <div className="mx-auto w-[280px] rounded-3xl bg-white p-4 ring-1 ring-inset ring-zinc-900/7.5 group-hover:ring-zinc-900/10 dark:bg-zinc-900 dark:ring-white/10 dark:group-hover:ring-white/20">
+        <BackgroundGradient className="mx-auto w-[280px] rounded-[22px] bg-white p-4 ring-1 ring-inset ring-zinc-900/7.5 dark:bg-zinc-900">
+          <a href="https://tofupilot.com" target="_blank">
             <div className="flex justify-between">
               <p className="text-2xs text-zinc-600 dark:text-zinc-400">
                 Docs by the TofuPilot team
@@ -103,10 +81,13 @@ export function TofuPilotBanner() {
               <XMarkIcon
                 aria-hidden="true"
                 className="h-4 w-4 cursor-pointer text-zinc-300 dark:text-zinc-700"
-                onClick={() => setIsOpen(false)}
+                onClick={(event) => {
+                  event.preventDefault()
+                  event.stopPropagation()
+                  setIsOpen(false)
+                }}
               />
             </div>
-
             <h3 className="mt-2 text-sm font-semibold text-zinc-900 dark:text-white">
               Looking for plug-and-play <br /> OpenHTF database & analytics?
             </h3>
@@ -121,13 +102,11 @@ export function TofuPilotBanner() {
                   className="h-4 w-4"
                 />
               </div>
-              <a href="https://tofupilot.com" target="_blank">
-                <Button variant="text" arrow="right">
-                  Try TofuPilot
-                </Button>
-              </a>
+              <Button variant="text" arrow="right">
+                Try TofuPilot
+              </Button>
             </div>
-          </div>
+          </a>
         </BackgroundGradient>
       </div>
     </div>

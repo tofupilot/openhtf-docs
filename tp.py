5f4d4ff06cb6f190b21aa3e3177f0212c63a55bb
Julien Buteau <julien@strake.one>
2024-10-18 12:02:00 +0200

    Cleaned-up execution files

--- tp.py
+++
@@ -1,7 +0,0 @@
-from tofupilot import TofuPilotClient
-
-client = TofuPilotClient(
-    api_key="24b0cd58-b72f-49ed-89cc-c022d05b5b30", base_url="http://localhost:3000"
-)
-
-client.import_log("./test_result2.json")
